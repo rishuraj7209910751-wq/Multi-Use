06e61c9c90497abe2d8ea15045d20ffb5d56ea7a
rishuraj7209910751-wq <rishuraj7209910751@gmail.com>
2026-01-22 22:10:41 +0530

    Update environment variable defaults in config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = int(os.environ.get("API_ID", "10811400"))
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522')) 
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-CAPTION = os.environ.get("CAPTION", "")
+API_ID = int(os.environ.get("API_ID", ""))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", '6286894502')) 
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "LunaBotCreator")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "Luna_Flix_Chat")
+CAPTION = os.environ.get("CAPTION", "hi")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
-group = environ.get('GROUP', '-1002173560131')
+group = environ.get('GROUP', '-1002987928500')
 GROUP = int(group) if group and id_pattern.search(group) else None
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '6286894502'))
 
 
 
